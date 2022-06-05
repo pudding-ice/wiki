70a70e91d158e0bf4792e9c10d32292fc01a8ac9
guochun <1076294651@qq.com>
2022-06-05 18:05:28 +0800

    22. 将ant-design-Vue的list示例代码放到电子书页面中

--- web/src/views/EbookView.vue
+++ web/src/views/EbookView.vue
@@ -1,6 +1,86 @@
 <template>
       <a-layout-content :style="{ padding: '0 24px', minHeight: '280px' }">
-        Content
+        <a-list :data-source="listData" :pagination="pagination" item-layout="vertical" size="large">
+          <template #footer>
+            <div>
+              <b>ant design vue</b>
+              footer part
+            </div>
+          </template>
+          <template #renderItem="{ item }">
+            <a-list-item key="item.title">
+              <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component :is="type" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+              </template>
+              <template #extra>
+                <img
+                    alt="logo"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                    width="272"
+                />
+              </template>
+              <a-list-item-meta :description="item.description">
+                <template #title>
+                  <a :href="item.href">{{ item.title }}</a>
+                </template>
+                <template #avatar>
+                  <a-avatar :src="item.avatar"/>
+                </template>
+              </a-list-item-meta>
+              {{ item.content }}
+            </a-list-item>
+          </template>
+        </a-list>
       </a-layout-content>
 </template>
+
+<script lang="ts">
+import {LikeOutlined, MessageOutlined, StarOutlined} from '@ant-design/icons-vue';
+import {defineComponent} from 'vue';
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
+export default defineComponent({
+  components: {
+    StarOutlined,
+    LikeOutlined,
+    MessageOutlined,
+  },
+  setup() {
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions: Record<string, string>[] = [
+      {type: 'StarOutlined', text: '156'},
+      {type: 'LikeOutlined', text: '156'},
+      {type: 'MessageOutlined', text: '2'},
+    ];
+    return {
+      listData,
+      pagination,
+      actions,
+    };
+  },
+});
+</script>
+
+
 
