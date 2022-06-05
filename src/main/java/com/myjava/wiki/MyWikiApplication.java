b73248e5c32c89e156bb4c741863711cb6554bc3
guochun <1076294651@qq.com>
2022-06-05 09:19:18 +0800

    1. 创建项目

---
+++ src/main/java/com/myjava/wiki/MyWikiApplication.java
@@ -0,0 +1,13 @@
+package com.myjava.wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWikiApplication.class, args);
+    }
+
+}
