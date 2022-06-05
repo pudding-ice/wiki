b1db7218ec71e4dd5504873348c07263b955b141
guochun <1076294651@qq.com>
2022-06-05 20:27:41 +0800

    30. 通过在main.ts中设置axios的URL,使得再使用axios的get或者post请求的时候不必拼接服务器地址

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,7 +5,9 @@ import store from './store'
 import ant from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
-
+//修改axios默认请求URL
+// import axios from 'axios'
+// axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 const app = createApp(App)
 app.use(store).use(router).use(ant).mount('#app')
 //全局使用图标
