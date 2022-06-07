985eea9c50d459f1431a3aa203415a16def4b0cc
guochun <1076294651@qq.com>
2022-06-07 10:37:09 +0800

    38. 编写电子书管理页面

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,217 @@
 <template>
-  <div class="about">
-    <h1>电子书管理页面</h1>
-  </div>
+  <a-layout>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <p>
+        <a-form :model="param" layout="inline">
+          <a-form-item>
+            <a-input v-model:value="param.name" placeholder="名称">
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="handleQuery({current: 1, pageSize: pagination.pageSize})">
+              查询
+            </a-button>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="add()">
+              新增
+            </a-button>
+          </a-form-item>
+        </a-form>
+      </p>
+      <a-table
+          :columns="columns"
+          :data-source="ebooks"
+          :loading="loading"
+          :pagination="pagination"
+      >
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar"/>
+        </template>
+        <!--        <template v-slot:category="{ text, record }">-->
+        <!--          <span>{{ getCategoryName(record.category1Id) }} / {{ getCategoryName(record.category2Id) }}</span>-->
+        <!--        </template>-->
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <!--            <router-link :to="'/admin/doc?ebookId=' + record.id">-->
+            <!--              <a-button type="primary">-->
+            <!--                文档管理-->
+            <!--              </a-button>-->
+            <!--            </router-link>-->
+            <a-button type="primary" @click="edit(record)">
+              编辑
+            </a-button>
+            <a-popconfirm
+                cancel-text="否"
+                ok-text="是"
+                title="删除后不可恢复，确认删除?"
+                @confirm="handleDelete(record.id)"
+            >
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-popconfirm>
+          </a-space>
+        </template>
+      </a-table>
+    </a-layout-content>
+  </a-layout>
+
+  <a-modal
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      title="电子书表单"
+      @ok="handleModalOk"
+  >
+    <a-form :label-col="{ span: 6 }" :model="ebook" :wrapper-col="{ span: 18 }">
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover"/>
+        <a-upload
+            v-model:file-list="fileList"
+            :action="SERVER + '/ebook/upload/avatar'"
+            :before-upload="beforeUpload"
+            :show-upload-list="false"
+            class="avatar-uploader"
+            list-type="picture-card"
+            name="avatar"
+            @change="handleChange"
+        >
+          <img v-if="imageUrl" :src="imageUrl" alt="avatar"/>
+          <div v-else>
+            <loading-outlined v-if="coverLoading"></loading-outlined>
+            <plus-outlined v-else></plus-outlined>
+            <div class="ant-upload-text">Upload</div>
+          </div>
+        </a-upload>
+      </a-form-item>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name"/>
+      </a-form-item>
+      <a-form-item label="分类">
+        <a-cascader
+            v-model:value="categoryIds"
+            :field-names="{ label: 'name', value: 'id', children: 'children' }"
+            :options="level1"
+        />
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.description" type="textarea"/>
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
+
+
+<script lang="ts">
+import {defineComponent, onMounted, ref} from 'vue';
+import {message} from 'ant-design-vue';
+import axiosRequest from "@/api/axiosRequest";
+
+export default defineComponent({
+  name: 'AdminEbook',
+  setup() {
+    const param = ref();
+    param.value = {};
+    const ebooks = ref();
+    const pagination = ref({
+      current: 1,
+      pageSize: 10,
+      total: 0
+    });
+    const loading = ref(false);
+
+    const columns = [
+      {
+        title: '封面',
+        dataIndex: 'cover',
+        slots: {customRender: 'cover'}
+      },
+      {
+        title: '名称',
+        dataIndex: 'name'
+      },
+      {
+        title: '分类',
+        slots: {customRender: 'category'}
+      },
+      {
+        title: '文档数',
+        dataIndex: 'docCount'
+      },
+      {
+        title: '阅读数',
+        dataIndex: 'viewCount'
+      },
+      {
+        title: '点赞数',
+        dataIndex: 'voteCount'
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        slots: {customRender: 'action'}
+      }
+    ];
+
+    onMounted(() => {
+      handleQuery({
+        current: 1,
+        pageSize: pagination.value.pageSize,
+      });
+    })
+    /**
+     * 数据查询
+     **/
+    const handleQuery = (params: any) => {
+      loading.value = true;
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+      ebooks.value = [];
+      axiosRequest.fetchGet('/ebook/getList', {
+        current: params.current,
+        pageSize: params.pageSize,
+        name: param.value.name
+      }).then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.respMessage.success) {
+          ebooks.value = data.dataList;
+          // 重置分页按钮
+          pagination.value.current = params.current;
+          pagination.value.total = data.total;
+        } else {
+          message.error(data.respMessage.message);
+        }
+      });
+    };
+
+
+    const SERVER = process.env.VUE_APP_SERVER;
+    const fileList = ref([]);
+    const coverLoading = ref<boolean>(false);
+    const imageUrl = ref<string>('');
+
+    return {
+      param,
+      ebooks,
+      pagination,
+      columns,
+      loading,
+      handleQuery,
+      fileList,
+      coverLoading,
+      imageUrl,
+      SERVER
+    }
+  }
+});
+</script>
+
+
+<style scoped>
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
